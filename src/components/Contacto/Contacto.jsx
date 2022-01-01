5817079a2f7451d69b9330ec9c902f0e4ada067b
Julio_Cesar_Pariona_Ventura <Julio_P@hotmail.com>
2022-01-01 14:06:28 -0500

    se agrega la sección Blog y se da unos ajustes en los estilos css

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -4,71 +4,67 @@ import './Contacto.css'
 const Contacto = () => {
   return (
     <section id="contacto">
-      <div className="container">
-        <button className="main--collapse btn btn-primary" type="button">
-          <h2 className="article__title">Haga su consulta aquí</h2>
-        </button>
-        <form>
-          <div className="d-flex flex-column align-items-start">
-            <div className="d-flex my-3 justify-content-between w-100">
-              <div className="form-floating">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="contactName"
-                  placeholder="Nombre *"
-                  required
-                />
-                <label htmlFor="contactName">Nombre</label>
-              </div>
-              <div className="form-floating">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="contactLastName"
-                  placeholder="Apellido *"
-                  required
-                />
-                <label htmlFor="contactLastName">Apellido</label>
-              </div>
+      <h2 className="main__title container">Haga su consulta aquí</h2>
+      <div className="p-5 mt-4">
+        <div className="container">
+          <form className="form">
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control"
+                id="contactName"
+                placeholder="Nombre *"
+                required
+              />
+              <label htmlFor="contactName">Nombre *</label>
             </div>
-            <div className="d-flex my-3 justify-content-between w-100">
-              <div className="form-floating">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="contactEmail"
-                  placeholder="Email *"
-                  required
-                />
-                <label htmlFor="contactEmail">Email</label>
-              </div>
-              <div className="form-floating">
-                <input
-                  type="tel"
-                  className="form-control"
-                  id="contactTel"
-                  placeholder="Telefono *"
-                  required
-                />
-                <label htmlFor="contactTel">Telefono</label>
-              </div>
+            <div className="form-floating">
+              <input
+                type="text"
+                className="form-control"
+                id="contactLastName"
+                placeholder="Apellido *"
+                required
+              />
+              <label htmlFor="contactLastName">Apellido *</label>
             </div>
-            <label htmlFor="contactMessage" className="d-none">
-              Example textarea
-            </label>
-            <textarea
-              placeholder="Escriba su consulta"
-              id="contactMessage"
-              cols="30"
-              rows="5"
-              className="form-control my-3"
-            ></textarea>
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control"
+                id="contactEmail"
+                placeholder="Email *"
+                required
+              />
+              <label htmlFor="contactEmail">Email *</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="tel"
+                className="form-control"
+                id="contactTel"
+                placeholder="Telefono *"
+                required
+              />
+              <label htmlFor="contactTel">Telefono *</label>
+            </div>
+            <div className="form__textarea">
+              <label htmlFor="contactMessage" className="d-none">
+                Example textarea
+              </label>
+              <textarea
+                placeholder="Escriba su consulta"
+                id="contactMessage"
+                cols="30"
+                rows="5"
+                className="form-control my-3"
+              ></textarea>
+            </div>
             <button type="submit" className="btn btn-contacto my-3">
               ENVIAR
             </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </section>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,51 +1,31 @@
 import * as React from "react";
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import './Main.css'
 
 const Main = () => {
   return (
-    <div className="container">
-      {/* Seccion que ofrecemos */}
+    <main className="container">
+      {/* Sección que ofrecemos */}
       <article id="que_ofrecemos" className="main__article">
-        <button
-          className="main--collapse btn btn-primary"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_que_ofrecemos"
-          aria-controls="collapse_que_ofrecemos"
-        >
-          <h2 className="article__title">¿Qué ofrecemos?</h2>
-        </button>
-        <section
-          className="main--collapsed collapse show"
-          id="collapse_que_ofrecemos"
-        >
-          <p className="card card-body article__text">
-            Contador Simple ofrece una amplia gama de servicios de consultoría
-            en materia impositiva, financiera y de administración. Trabajamos
-            junto con nuestros clientes de principio a fin centrándonos en sus
-            necesidades a la vez que producimos nuevas ideas, desarrollamos
-            estrategias efectivas y diseñamos solucionaes escalables de alta
-            calidad.
-            <span className="bold">Contáctenos para obtener más información. </span>
-          </p>
-        </section>
+        <h2 className="main__title">¿Qué ofrecemos?</h2>
+        <p className="p-3 article__text">
+          Contador Simple ofrece una amplia gama de servicios de consultoría
+          en materia impositiva, financiera y de administración. Trabajamos
+          junto con nuestros clientes de principio a fin centrándonos en sus
+          necesidades a la vez que producimos nuevas ideas, desarrollamos
+          estrategias efectivas y diseñamos solucionaes escalables de alta
+          calidad.
+          <span className="bold">Contáctenos para obtener más información. </span>
+        </p>
       </article>
-      {/* Seccion quienes somos  */}
-      <article id="quienes_somos" className="main__article">
-        <button
-          className="main--collapse btn btn-primary"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_quienes_somos"
-          aria-controls="collapse_quienes_somos"
-        >
-          <h2 className="article__title">¿Quiénes somos?</h2>
-        </button>
-        <section
-          className="main--collapsed collapse show"
-          id="collapse_quienes_somos"
-        >
-          <p className="card card-body article__text">
+      {/* Sección quienes somos */}
+      <section id="quienes_somos" className="main__article">
+        <div className="row py-3">
+          <img src={require("../../media/about-us.png")} alt="personas fisicas" />
+        </div>
+        <article className="mt-2">
+          <h2 className="main__title">¿Quiénes somos?</h2>
+          <p className="p-3 pb-0 article__text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh id
             tellus odio quis. Ullamcorper in condimentum aliquet quam vel
             aliquam ornare. Imperdiet pharetra risus proin sed. Molestie massa
@@ -54,7 +34,7 @@ const Main = () => {
             morbi iaculis. Dictumst quisque netus quam nec aliquet. Tellus fusce
             purus urna sagittis.
           </p>
-          <p className="card card-body article__text">
+          <p className="p-3  article__text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh id
             tellus odio quis. Ullamcorper in condimentum aliquet quam vel
             aliquam ornare. Imperdiet pharetra risus proin sed. Molestie massa
@@ -63,44 +43,32 @@ const Main = () => {
             morbi iaculis. Dictumst quisque netus quam nec aliquet. Tellus fusce
             purus urna sagittis.
           </p>
-        </section>
-      </article>
+        </article>
+      </section>
 
-      {/* Seccion servicios */}
+      {/* Sección servicios */}
       <section id="servicios" className="main__article">
-        <button
-          className="main--collapse btn btn-primary"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_servicios"
-          aria-controls="collapse_servicios"
-        >
-          <h2 className="article__title">¿Qué podemos hacer por usted?</h2>
-        </button>
+        <h2 className="main__title">¿Qué podemos hacer por usted?</h2>
         <section
           className="
             main--collapsed
-            collapse
-            show
             d-flex
             justify-content-evenly
-            flex-column
-            mx-5
+            flex-wrap
           "
-          id="collapse_servicios"
         >
           <div
             className="
               d-flex
               flex-column
-              justify-content-center
               align-items-center
-              mt-5
+              my-5
+              px-3
             "
           >
-            <img src={require("../../media/persona_fisica.png")} alt="personas fisicas" />
+            <img src={require("../../media/persona_fisica.png")} className="w-100" alt="personas fisicas" />
             <h3 className="my-4">PERSONAS FÍSICAS</h3>
-            <p>
+            <p className="text-align-justify">
               <span className="d-block">Asesoramiento Tributario.</span>
               Ahorra con una mejor planificación fiscal y disfruta de lo que más
               te gusta hacer, que nosotros nos encargamos de tus impuestos.
@@ -113,14 +81,14 @@ const Main = () => {
             className="
               d-flex
               flex-column
-              justify-content-center
               align-items-center
-              mt-5
+              my-5
+              px-3
             "
           >
-            <img src={require("../../media/pymes.png")} alt="pymes" />
+            <img src={require("../../media/pymes.png")} className="w-100" alt="pymes" />
             <h3 className="my-4">PYMES</h3>
-            <p>
+            <p className="text-align-justify">
               <span className="d-block">Haga crecer su negocio.</span> Contador
               Simple te asesora y te apoya en todo el recorrido. Nuestros
               servicios flexibles y personalizados a cada empresa o negocio se
@@ -130,7 +98,25 @@ const Main = () => {
           </div>
         </section>
       </section>
-    </div>
+
+      {/* Sección nuestro Blog */}
+      <section id="blog" className="main__article">
+        <h2 className="main__title">Nuestro Blog</h2>
+        <section className="p-4 m-md-5 mx-2 my-5 position-relative border border-dark">
+          <h3 className="">Lorem Impsum</h3>
+          <p className="article__text mr-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh id
+            tellus odio quis. Ullamcorper in condimentum aliquet quam vel
+            aliquam ornare. Imperdiet pharetra risus proin sed. Molestie massa
+            urna gravida fringilla vulputate ultrices egestas. Eget egestas
+            proin nunc, proin sodales congue. A massa pretium nisl, risus leo
+            morbi iaculis. Dictumst quisque netus quam nec aliquet. Tellus fusce
+            purus urna sagittis.
+          </p>
+          <i className="icon-heart"><span><AiOutlineHeart/></span><span><AiFillHeart/></span></i>
+        </section>
+      </section>
+    </main>
   );
 };
 
