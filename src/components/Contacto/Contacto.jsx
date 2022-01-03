fb12c210bd29b9b361db4b48888851e50d281cdd
Julio_Cesar_Pariona_Ventura <Julio_P@hotmail.com>
2022-01-02 20:25:31 -0500

    se agrega funcionalidad al formulario

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -2,21 +2,76 @@ import * as React from "react";
 import './Contacto.css'
 
 const Contacto = () => {
+  const [userData, setUserData] = React.useState({}); // obtiene los datos de los inputs.
+  const [btnVar, setBtnVar] = React.useState(false);
+  const [userNameData, setUserNameData] = React.useState("");
+  const [userLastNameData, setUserLastNameData] = React.useState("");
+  const [userEmailData, setUserEmailData] = React.useState("");
+  const [userPhoneData, setUserPhoneData] = React.useState("");
+  const [userTextData, setUserTextData] = React.useState("");
+
+  // comprueba la validacion de los inputs.
+  const handleSubmit = (e) => {
+    let form = document.querySelector('.needs-validation');
+    form.classList.add('was-validated');
+    e.preventDefault();
+    e.stopPropagation();
+    if (form.checkValidity()) {
+      form.classList.remove('was-validated');
+      setUserData({name: userNameData, lastName: userLastNameData, email: userEmailData, phone: userPhoneData, text: userTextData});
+      setUserNameData("");
+      setUserLastNameData("");
+      setUserEmailData("");
+      setUserPhoneData("");
+      setUserTextData("");
+      alert("se enviaron los datos correctamente.");
+    } 
+  }
+
+  // asegura que no se exceda el numero de caracteres.
+  const handleChangePhone = (e) => {
+    if(userPhoneData.length < 9) {
+      setUserPhoneData(e.target.value);
+    }
+    if(userPhoneData.length > 8) {
+      if(btnVar) {
+        setUserPhoneData(e.target.value);
+      }
+    }
+  }
+
+  // asegura que se pueda borrar algun caracter e inhabilita el teclado "espacio".
+  const handleKey = (e) => {
+    if(e.keyCode === 8) {
+      setBtnVar(true);
+      setTimeout(() => {
+        setBtnVar(false);
+      }, 100);
+    } 
+    if(e.keyCode === 32) {
+      e.preventDefault();
+    }
+  }
+
   return (
     <section id="contacto">
       <h2 className="main__title container">Haga su consulta aquí</h2>
       <div className="p-5 mt-4">
         <div className="container">
-          <form className="form">
+          <form className="form needs-validation" onSubmit={handleSubmit}>
             <div className="form-floating">
               <input
                 type="text"
                 className="form-control"
                 id="contactName"
                 placeholder="Nombre *"
+                value={userNameData}
+                onChange={(e) => setUserNameData(e.target.value)}
                 required
               />
               <label htmlFor="contactName">Nombre *</label>
+              <span className="valid-feedback">Ok!</span>
+              <span className="invalid-feedback">Debe llenar correctamente los espacios.</span>
             </div>
             <div className="form-floating">
               <input
@@ -24,9 +79,13 @@ const Contacto = () => {
                 className="form-control"
                 id="contactLastName"
                 placeholder="Apellido *"
+                value={userLastNameData}
+                onChange={(e) => setUserLastNameData(e.target.value)}
                 required
               />
               <label htmlFor="contactLastName">Apellido *</label>
+              <span className="valid-feedback">Ok!</span>
+              <span className="invalid-feedback">Debe llenar correctamente los espacios.</span>
             </div>
             <div className="form-floating">
               <input
@@ -34,9 +93,14 @@ const Contacto = () => {
                 className="form-control"
                 id="contactEmail"
                 placeholder="Email *"
+                value={userEmailData}
+                onChange={(e) => setUserEmailData(e.target.value)}
+                onKeyDown={(e) => handleKey(e)}
                 required
               />
               <label htmlFor="contactEmail">Email *</label>
+              <span className="valid-feedback">Ok!</span>
+              <span className="invalid-feedback">Debe llenar correctamente los espacios.</span>
             </div>
             <div className="form-floating">
               <input
@@ -44,9 +108,14 @@ const Contacto = () => {
                 className="form-control"
                 id="contactTel"
                 placeholder="Telefono *"
+                value={userPhoneData}
+                onChange={(e) => handleChangePhone(e)}
+                onKeyDown={(e) => handleKey(e)}
                 required
               />
               <label htmlFor="contactTel">Telefono *</label>
+              <span className="valid-feedback">Ok!</span>
+              <span className="invalid-feedback">Debe llenar correctamente los espacios.</span>
             </div>
             <div className="form__textarea">
               <label htmlFor="contactMessage" className="d-none">
@@ -58,6 +127,8 @@ const Contacto = () => {
                 cols="30"
                 rows="5"
                 className="form-control my-3"
+                value={userTextData}
+                onChange={(e) => setUserTextData(e.target.value)}
               ></textarea>
             </div>
             <button type="submit" className="btn btn-contacto my-3">
